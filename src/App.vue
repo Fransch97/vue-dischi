961f48a699faa970213f4970dd74eb27ec0398e2
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-17 15:39:30 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,6 @@ export default {
 </script>
 
 <style lang="scss">
+@import "./assets/styles/global";
 
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,25 @@
 <template>
   <header>
-
+    <div class="logo">
+      <img :src="img" alt="">
+      </div>
+    <select class="form-select sele-sc" aria-label="Default select example">
+      <option selected>Seleziona un genere</option>
+    </select> 
   </header>
 </template>
 
 <script>
 export default {
-    name: "AppHeader"
+    name: "AppHeader",
+    data(){
+      return{
+        img: require("../assets/img/spt.png")
+      }
+    }
 }
 </script>
 
-<style>
-
+<style lang="scss" >
+@import "../assets/styles/header"
 </style>
