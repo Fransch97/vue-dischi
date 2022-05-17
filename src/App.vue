24c57d967b88c6f2391b05f8d391c34e00f9f341
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-17 15:22:54 +0200

    starting header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
+    <AppHeader/>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-
+    components: { AppHeader }
 }
 </script>
 
