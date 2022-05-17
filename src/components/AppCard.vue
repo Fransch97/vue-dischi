870f7fef312c8b7dc7fcb5920e9eef4b482683a9
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-17 16:02:34 +0200

    cards

---
+++ src/components/AppCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card-sc">
+      <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+      <p>New Jersey</p>
+      <span>Bon Jovi</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import "../assets/styles/card";
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,37 +1,48 @@
 <template>
 <main>
-    
+    <div class="container main">
+        <AppCard/>
+    </div>
 </main>
   
 </template>
 
 <script>
 import  axios from "axios";
+import AppCard from "./AppCard.vue";
 export default {
     name: "AppMain",
-    comments:"axios",
+    comments: "axios",
     data() {
         return {
             url: "https://flynn.boolean.careers/exercises/api/array/music",
             cards: Array
-        }
+        };
     },
     methods: {
-        api(){
+        api() {
             axios.get(this.url)
-            .then(api =>{
-                console.log(api.data.response)
-                this.cards = api.data.response
-                console.log(this.cards ,"le card")
-            })
+                .then(api => {
+                console.log(api.data.response);
+                this.cards = api.data.response;
+                console.log(this.cards, "le card");
+            });
         }
     },
     mounted() {
-        this.api()
+        this.api();
     },
+    components: { AppCard }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+main{
+    padding: 50px;
+    .main{
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+}
 </style>
